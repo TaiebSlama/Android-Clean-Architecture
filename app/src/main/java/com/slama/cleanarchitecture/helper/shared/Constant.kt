c1721f9f82513e9d6d7239015088e2d2375d4cfd
Slama Taieb <slama.taieb.contact@gmail.com>
2022-10-01 17:41:06 +0100

    clean code

--- app/src/main/java/com/slama/cleanarchitecture/helper/shared/Constant.kt
+++ app/src/main/java/com/slama/cleanarchitecture/helper/shared/Constant.kt
@@ -10,19 +10,7 @@ class Constant {
 
     class Global {
         companion object {
-            const val STRING_EMPTY = ""
-            const val BLANK_STRING = "********"
-            const val ZERO = "0.0"
-            const val ZERO_WITHOUT_COMMA = "0"
-            const val ONE = "1"
-            const val NO_VALUE = "----"
             const val NAVIGATION_USE_CASE_KEY = "useCase"
-            const val TVA = 0.19
-            const val DATE_FORMAT = "yyyy/M/dd hh:mm:ss"
-            const val PERIOD_DAILY_WEEKLY_REPORT_FORMAT = "yyyy/M/dd"
-            const val PERIOD_MONTHLY_REPORT_FORMAT = "M/yyyy"
-            const val DATE_FORMAT_CALENDAR = "EEEE, d MMMM yyyy"
-
         }
     }
 
